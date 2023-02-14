89d05415f691a288c43d01035d72131bfbc5dc63
Sushmitha S <sushmitha9724@yahoo.com>
2023-02-14 20:42:58 +0530

    Add database tables,migrations ,partial view

---
+++ LeaveManagement.Web/Data/LeaveType.cs
@@ -0,0 +1,15 @@
+﻿namespace LeaveManagement.Web.Data
+{
+    public class LeaveType: BaseEntity
+    {
+        
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+   
+        
+
+    
+    
+    
+    }
+}
--- LeaveManagement.Web/Program.cs
+++ LeaveManagement.Web/Program.cs
@@ -1,7 +1,17 @@
+using LeaveManagement.Web.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(connectionString));
+//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddDefaultIdentity<Employee>(options=>options.SignIn.RequireConfirmedAccount=true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
 
 var app = builder.Build();
 
